35ddb555d465a0e1637a98a03e7ba3f4c811ae68
Thalles Rodrigues Herminio <20191194010007@ifrn.local>
2022-11-24 08:28:49 -0300

    implementando registro

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import teste, T_inicial
+from core.views import teste, T_inicial, registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste/', teste ),
-    path('', T_inicial)
+    path('', T_inicial, name='inicial'),
+    path('registro/', registro)
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponsePermanentRedirect
 
 def teste(request):
     return render(request, 'teste.html')
 
 def T_inicial(request):
     return render(request, 'inicial.html')
+
+def registro(request):
+
+    # if form.is_valid():
+    #     form.save()
+    #     return redirect('inicio')
+
+    return render(request, 'registro.html')
